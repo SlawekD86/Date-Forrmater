86e7ee1be6bd326c5f8f6b8c4bb9fd017af72a25
Slawek Derabin <slawomirderabin@gmail.com>
2024-03-10 15:42:07 +0100

    step 3

--- script.js
+++ script.js
@@ -1,11 +1,21 @@
 
 const currentDateParagraph = document.getElementById('current-date');
+const dateOptionsSelectElement = document.getElementById('date-options');
 
 /* 
 
-Step 2
-Use the .getElementById() method to get the #date-options element and use const to assign it to a variable named dateOptionsSelectElement.
+Step 3
+In JavaScript, there are many built-in constructors that create objects. A constructor is like a regular function, but starts with a capital letter, and is initialized with the new operator.
+
+For example, you can use the Date() constructor with the new operator to create a new Date object that returns a string with the current date and time:
+
+const currentDate = new Date();
+console.log(currentDate);
+
+// Output:
+// Mon Aug 23 2021 15:31:00 GMT-0400 (Eastern Daylight Time)
+Create a new const variable called date and assign it a Date object with new Date().
 
 */
 
-const dateOptionsSelectElement = document.getElementById('date-options');
+const date = new Date();
