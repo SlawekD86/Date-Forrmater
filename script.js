d7d98a928b4b70bf0e12cba0131ea89ebc3ae03c
Slawek Derabin <slawomirderabin@gmail.com>
2024-03-10 15:50:57 +0100

    step 8

--- script.js
+++ script.js
@@ -6,14 +6,15 @@ const date = new Date();
 const day = date.getDate();
 const month = date.getMonth() + 1;
 const year = date.getFullYear();
+const hour = date.getHours();
 
 /* 
 
-Step 7
-The .getHours() method returns a number between 0 and 23. This represents the hour for the provided date, where 0 is midnight and 23 is 11 p.m.
+Step 8
+The .getMinutes() method returns a number between 0 and 59 which represents the minutes for the provided date.
 
-Create a const variable named hours and assign it the hour from date with the .getHours() method.
+Create a const variable named minutes and assign it the minutes from date with the .getMinutes() method.
 
 */
 
-const hour = date.getHours();
+const minutes = date.getMinutes();
