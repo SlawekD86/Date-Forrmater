0e9748b63e740a9d4bb4b282f21d13e8dc027d0e
Slawek Derabin <slawomirderabin@gmail.com>
2024-03-10 15:17:12 +0100

    step 1

---
+++ script.js
@@ -0,0 +1,12 @@
+/* 
+
+Step 1
+In this project, you will learn about the JavaScript Date object by building a date formatter.
+
+All of the HTML and CSS for this project have been provided for you.
+
+Start by getting the #current-date element using the .getElementById() method and assign it to a const variable called currentDateParagraph.
+
+*/
+
+const currentDateParagraph = document.getElementById('current-date');
