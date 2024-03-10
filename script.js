8110f76f1d5c8a9337a7283daa1dd9ee2e4dd565
Slawek Derabin <slawomirderabin@gmail.com>
2024-03-10 16:02:45 +0100

    step 14

--- script.js
+++ script.js
@@ -11,12 +11,12 @@ const minutes = date.getMinutes();
 
 /* 
 
-Step 13
-To see the results of the formattedDate variable, add a console.log() statement and pass in the formattedDate variable as an argument.
+Step 14
+Use the textContent property on currentDateParagraph to set its text content to the value of the formattedDate variable.
 
-Open up the console and you should see the date printed out.
+Also, make sure to remove your console.log(formattedDate); line.
 
 */
 
 const formattedDate = `${day}-${month}-${year}`;
-console.log(formattedDate);
+currentDateParagraph.textContent = formattedDate;
