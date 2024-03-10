52e1ddf80c24198a3239ed45acf2e1cfa3543c20
Slawek Derabin <slawomirderabin@gmail.com>
2024-03-10 16:37:46 +0100

    step 24

--- script.js
+++ script.js
@@ -14,8 +14,25 @@ currentDateParagraph.textContent = formattedDate;
 
 /* 
 
-Step 23
-Inside the case for mm-dd-yyyy-h-mm, set the textContent property of currentDateParagraph to ${month}-${day}-${year} ${hours} Hours ${minutes} Minutes.
+Step 24
+In a switch statement, the default case is executed when none of the previous case conditions match the value being evaluated. It serves as a catch-all for any other possible cases. For example:
+
+const dayOfWeek = 7;
+
+switch (dayOfWeek) {
+  case 1:
+    console.log("It's Monday!");
+    break;
+  case 2:
+    console.log("It's Tuesday!");
+    break;
+  // ...cases for other workdays
+  default:
+    console.log("It's the weekend!");
+}
+For the default case, set the text content of currentDateParagraph to the value of formattedDate.
+
+And with that, your date formatter is complete! You can now format the current date three different ways.
 
 */
 
@@ -28,7 +45,9 @@ dateOptionsSelectElement.addEventListener('change', () => {
                 .join('-');
             break;
         case 'mm-dd-yyyy-h-mm':
-            currentDateParagraph.textContent = `${month}-${day}-${year} ${hour} Hours ${minutes} Minutes`;
+            currentDateParagraph.textContent = `${month}-${day}-${year}  ${hour} : ${minutes} `;
             break;
+        default:
+            currentDateParagraph.textContent = formattedDate;
     };
 });
