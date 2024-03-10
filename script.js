07ebe166c305a8b54535f3559f110ac6e46781c9
Slawek Derabin <slawomirderabin@gmail.com>
2024-03-10 16:20:07 +0100

    step 18

--- script.js
+++ script.js
@@ -14,22 +14,15 @@ currentDateParagraph.textContent = formattedDate;
 
 /* 
 
-Step 17
-When the user chooses the Year, Month, Day option from the dropdown, the date format should reflect this choice.
-
-To do this, you can add a case clause in the switch statement that checks for a match against the expression expr, followed by code to run if there's a match. Here's an example where the case clause checks that expr is equal to the string case123:
-
-switch (expr) {
-  case 'case123':
-    // Write your logic here
-}
-Add a case where the value is yyyy-mm-dd. Inside the case, set the text content of currentDateParagraph to the value of formattedDate.
+Step 18
+Split formattedDate into an array of substrings with the .split() method and use a "-" as the separator.
 
 */
 
 dateOptionsSelectElement.addEventListener('change', () => {
     switch (dateOptionsSelectElement.value) {
         case 'yyyy-mm-dd':
-            currentDateParagraph.textContent = formattedDate;
+            currentDateParagraph.textContent = formattedDate.split('-');
+
     };
 });
