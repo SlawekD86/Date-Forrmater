2c9593fcee1818ae19b630b436bb9ebdd3e987f0
Slawek Derabin <slawomirderabin@gmail.com>
2024-03-10 16:06:55 +0100

    step 15

--- script.js
+++ script.js
@@ -9,14 +9,19 @@ const year = date.getFullYear();
 const hour = date.getHours();
 const minutes = date.getMinutes();
 
+const formattedDate = `${day}-${month}-${year}`;
+currentDateParagraph.textContent = formattedDate;
+
 /* 
 
-Step 14
-Use the textContent property on currentDateParagraph to set its text content to the value of the formattedDate variable.
+Step 15
+In JavaScript, the change event is used to detect when the value of an HTML element has changed:
 
-Also, make sure to remove your console.log(formattedDate); line.
+element.addEventListener("change", () => {
+    
+});
+Attach the addEventListener method to the dateOptionsSelectElement. The first argument of the event listener should be the string change and the second argument should be an empty arrow function.
 
 */
 
-const formattedDate = `${day}-${month}-${year}`;
-currentDateParagraph.textContent = formattedDate;
+dateOptionsSelectElement.addEventListener('change', () => { });
